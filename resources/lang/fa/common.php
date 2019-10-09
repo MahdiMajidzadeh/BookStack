8fc46a66714dae60a1e2bb2cc9f8813eb88cbc96
Mahdi Majidzadeh <mahdi.majidzadeh@gmail.com>
2019-10-09 15:50:55 +0330

    translate common file

---
+++ resources/lang/fa/common.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * Common elements found throughout many areas of BookStack.
+ */
+return [
+
+    // Buttons
+    'cancel' => 'لغو',
+    'confirm' => 'تایید',
+    'back' => 'قبل',
+    'save' => 'ذخیره',
+    'continue' => 'ادامه',
+    'select' => 'انتخاب',
+    'toggle_all' => 'تغییر همگی',
+    'more' => 'بیشتر',
+
+    // Form Labels
+    'name' => 'نام',
+    'description' => 'توضیحات',
+    'role' => 'نقش',
+    'cover_image' => 'تصویر کاور',
+    'cover_image_description' => 'ابعاد تصویر می‌بایست 440x 250px باشد.',
+    
+    // Actions
+    'actions' => 'عملیات',
+    'view' => 'نمایش',
+    'view_all' => 'نمایش همه',
+    'create' => 'ایجاد',
+    'update' => 'به‌روزرسانی',
+    'edit' => 'ویرایش',
+    'sort' => 'مرتب‌سازی',
+    'move' => 'جابه‌جایی',
+    'copy' => 'کپی',
+    'reply' => 'جواب‌دادن',
+    'delete' => 'حذف',
+    'search' => 'جستجو',
+    'search_clear' => 'پاک کردن جستجو',
+    'reset' => 'ریست',
+    'remove' => 'حذف',
+    'add' => 'افزودن',
+
+    // Sort Options
+    'sort_options' => 'تنظیمات مرتب‌سازی',
+    'sort_direction_toggle' => 'جهت مرتب‌سازی',
+    'sort_ascending' => 'مرتب‌سازی صعودی',
+    'sort_descending' => 'مرتب‌سازی نزولی',
+    'sort_name' => 'نام',
+    'sort_created_at' => 'تاریخ ایجاد',
+    'sort_updated_at' => 'تاریخ به‌روزرسانی',
+
+    // Misc
+    'deleted_user' => 'کاربر حذف شده',
+    'no_activity' => 'فعالیتی برای نمایش نیست.',
+    'no_items' => 'نتیجه‌ای وجود ندارد.',
+    'back_to_top' => 'برو بالا',
+    'toggle_details' => 'نمایش جزییات',
+    'toggle_thumbnails' => 'نمایش پیش‌نمایش‌ها',
+    'details' => 'جزییات',
+    'grid_view' => 'نمایش پنجره‌ای',
+    'list_view' => 'نمایش لیستی',
+    'default' => 'پیش‌فرض',
+    'breadcrumb' => 'مسیر صفحه',
+
+    // Header
+    'profile_menu' => 'منوی پروفایل',
+    'view_profile' => 'نمایش پروفایل',
+    'edit_profile' => 'ویرایش پروفایل',
+
+    // Layout tabs
+    'tab_info' => 'اطلاعات',
+    'tab_content' => 'محتوا',
+
+    // Email Content
+    'email_action_help' => 'اگر با کلیک کردن روی ":actionText" مشکل دارید، آدرس زیر را در مرورگر خود کپی و پیست کنید.',
+    'email_rights' => 'تمام حقوق محفوظ است.',
+];
