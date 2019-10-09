f442b0cb79c0544f802e9b496eb33c9620565e8d
Mahdi Majidzadeh <mahdi.majidzadeh@gmail.com>
2019-10-09 13:48:58 +0330

    translate activity

---
+++ resources/lang/fa/activities.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Activity text strings.
+ * Is used for all the text within activity logs & notifications.
+ */
+return [
+
+    // Pages
+    'page_create'                 => 'ایجاد صفحه',
+    'page_create_notification'    => 'صفحه با موفقیت ایجاد شد',
+    'page_update'                 => 'به‌روزرسانی صفحه',
+    'page_update_notification'    => 'صفحه با موفقیت به‌روزرسانی شد',
+    'page_delete'                 => 'حذف صفحه',
+    'page_delete_notification'    => 'صفحه با موفقیت حذف شد',
+    'page_restore'                => 'بازیابی صفحه',
+    'page_restore_notification'   => 'صفحه با موفقیت بازیابی شد',
+    'page_move'                   => 'جابه‌جایی صفحه',
+
+    // Chapters
+    'chapter_create'              => 'ایجاد فصل',
+    'chapter_create_notification' => 'فصل با موفقیت ایجاد شد',
+    'chapter_update'              => 'به‌روزرسانی فصل',
+    'chapter_update_notification' => 'فصل با موفقیت به‌روزرسانی شد',
+    'chapter_delete'              => 'حذف فصل',
+    'chapter_delete_notification' => 'فصل با موفقیت حذف شد',
+    'chapter_move'                => 'جابه‌جایی فصل',
+
+    // Books
+    'book_create'                 => 'ایجاد کتاب',
+    'book_create_notification'    => 'کتاب با موفقیت ایجاد شد',
+    'book_update'                 => 'به‌روزرسانی کتاب',
+    'book_update_notification'    => 'کتاب با موفقیت به‌روزرسانی شد',
+    'book_delete'                 => 'حذف کتاب',
+    'book_delete_notification'    => 'کتاب با موفقیت حذف شد',
+    'book_sort'                   => 'مرتب کردن کتاب',
+    'book_sort_notification'      => 'کتاب با موفقیت مرتب شد',
+
+    // Bookshelves
+    'bookshelf_create'            => 'ساختن قفسه',
+    'bookshelf_create_notification'    => 'قفسه با موفقیت ایجاد شد',
+    'bookshelf_update'                 => 'به‌روزرسانی قفسه',
+    'bookshelf_update_notification'    => 'قفسه با موفقیت به‌روسانی شد',
+    'bookshelf_delete'                 => 'حذف قفسه',
+    'bookshelf_delete_notification'    => 'قفسه با موفقیت حذف شد',
+
+    // Other
+    'commented_on'                => 'نظر گذاشته روی',
+];
