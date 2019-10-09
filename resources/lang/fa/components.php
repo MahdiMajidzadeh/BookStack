a96f38c9c6b59a48a6738405bf58746f7913947d
Mahdi Majidzadeh <mahdi.majidzadeh@gmail.com>
2019-10-09 16:03:19 +0330

    translate components

---
+++ resources/lang/fa/components.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Text used in custom JavaScript driven components.
+ */
+return [
+
+    // Image Manager
+    'image_select' => 'انتخاب تصویر',
+    'image_all' => 'همه',
+    'image_all_title' => 'دیدن همه تصاویر',
+    'image_book_title' => 'دیدن تصاویری که برای این کتاب آپلود شده است.',
+    'image_page_title' => 'دیدن تصاویری که برای این صفحه آپلود شده است.',
+    'image_search_hint' => 'جستجو تصاویر با نام',
+    'image_uploaded' => 'آپلودشده در  :uploadedDate',
+    'image_load_more' => 'بارگذاری بیشتر',
+    'image_image_name' => 'نام تصویر',
+    'image_delete_used' => 'این تصویر در صفحه‌های زیر استفاده شده است:',
+    'image_delete_confirm' => 'برای تایید دوباره روی دکمه حذف کلیک کنید.',
+    'image_select_image' => 'انتخاب تصویر',
+    'image_dropzone' => 'تصاویر را رها کنید یا برای بارگذاری اینجا را کلیک کنید',
+    'images_deleted' => 'تصاویر حذف شده است.',
+    'image_preview' => 'پیش‌نمایش تصاویر',
+    'image_upload_success' => 'تصویر با موفقیت آپلود شد.',
+    'image_update_success' => 'جزییات تصویر با موفقیت به‌روز شد.',
+    'image_delete_success' => 'تصویر با موفقیت حذف شد.',
+    'image_upload_remove' => 'حذف',
+
+    // Code Editor
+    'code_editor' => 'ویرایش کد',
+    'code_language' => 'زبان کد',
+    'code_content' => 'محتوای کد',
+    'code_save' => 'ذخیره کد',
+];
